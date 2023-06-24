88966139e2f49b4b02eddb2c4bc2a205174a91de
Stone Chen <chen.stonechen@gmail.com>
2023-06-24 15:04:41 -0400

    re arrange folder structure

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-cl.exe /O2 ray.cpp  /link libSDL\SDL2main.lib libSDL\SDL2.lib "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22000.0\um\x64\shell32.lib" /SUBSYSTEM:CONSOLE
+cl.exe /Zi ray.cpp  /link build\SDL2main.lib build\SDL2.lib "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22000.0\um\x64\shell32.lib" /SUBSYSTEM:CONSOLE /OUT:build\ray.exe
 
-start ray.exe
+start build\ray.exe
