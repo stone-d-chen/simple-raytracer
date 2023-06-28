091c0c60f29cbb1e179ca94d486ec81f35304f2d
Stone Chen <chen.stonechen@gmail.com>
2023-06-28 11:09:33 -0400

    multiply ray samples per loop

--- SDL2_ray.cc
+++ SDL2_ray.cc
@@ -19,10 +19,40 @@ int main(int ArgC, char **Args)
 
 #include "WorldInit.h" //just to get it out of the way
 
-    u32 loopcount = 0;
+
+    f32 FilmDist = 1.0;
+    f32 FilmW = 1.0;
+    f32 FilmH = 1.0;
+    if (Image.Width > Image.Height)
+    {
+        FilmH *= (f32)Image.Height / Image.Width;
+    }
+    if (Image.Width < Image.Height)
+    {
+        FilmW *= (f32)Image.Width / Image.Height;
+    }
+    f32 HalfFilmW = 0.5 * FilmW;
+    f32 HalfFilmH = 0.5 * FilmH;
+
+    f32 PixH = 1.0 / (f32)Image.Height;
+    f32 PixW = 1.0 / (f32)Image.Width;
+    f32 HalfPixH = 0.5 * PixH;
+    f32 HalfPixW = 0.5 * PixW;
+
+
+    v3f CameraP = v3f{ 0, -10, 1 };
+    v3f LookAt = v3f{ 0, 0, 0, };
+    v3f CameraZ = Normalize(CameraP - LookAt);
+    v3f CameraX = Normalize(Cross(v3f{ 0,0,1 }, CameraZ)); //right hand rule
+    v3f CameraY = Normalize(Cross(CameraZ, CameraX)); //right hand rule
+
+    u32 RaysPerPixel = 4;
+
 
     while(is_running == true)
     {
+
+        v3f RayOrigin = CameraP;
         u32 *Out = Image.Pixels;
         for(u32 Y = 0; Y < Image.Height; ++Y)
         {
@@ -30,12 +60,15 @@ int main(int ArgC, char **Args)
             for(u32 X = 0; X < Image.Width; ++X)
             {
                 f32 FilmX = -1.0 + 2.0 * X / (f32) Image.Width;
-                v3f RayOrigin = CameraP;
-                v3f RayDirection = Normalize(-CameraZ * FilmDist + CameraY * FilmY * HalfFilmH + CameraX * FilmX * HalfFilmW);
-                v3f color = Raycast(World, Image, RayOrigin, RayDirection);
-
-
-                *Out++ = ARGBPack(color);
+                v3f color = {};
+                for(u32 RayIndex = 0; RayIndex < RaysPerPixel; ++RayIndex)
+                {
+                    f32 OffY = (FilmY + HalfPixH) + HalfPixH * RandomBilateral(World.State);
+                    f32 OffX = (FilmX + HalfPixW) + HalfPixW * RandomBilateral(World.State);
+                    v3f RayDirection = Normalize(-CameraZ * FilmDist + CameraY * OffY * HalfFilmH + CameraX * OffX * HalfFilmW);
+                    color += Raycast(World, Image, RayOrigin, RayDirection);
+                }
+                *Out++ = ARGBPack(color/RaysPerPixel);
             }
         }
 
@@ -46,14 +79,11 @@ int main(int ArgC, char **Args)
 
         SDL_LockTexture(texture, NULL, &TextureMemory, &Pitch);
         memcpy(TextureMemory, (void*)Image.Pixels, Image.Width*Image.Height*sizeof(u32));
+
         SDL_UnlockTexture(texture);
-        // TODO: Look into SDL_LockTexture
         SDL_RenderCopyEx(renderer, texture, NULL, NULL, 0, NULL, SDL_FLIP_VERTICAL);
-        SDL_Delay(10);
-        loopcount++;
-        printf("loop %d \r", loopcount);
         SDL_RenderPresent(renderer);
-        SDL_RenderClear(renderer);
+        // SDL_RenderClear(renderer);
     }
     return(0);
 
